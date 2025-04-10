e310b3078f3a136d745e5c361563a8123fdf381f
emiliooarrig <earriaga@anahuac.mx>
2025-04-09 20:18:36 -0600

    Logica para agregar cliente a la DB

---
+++ modulos/clientes/agregar_cliente.php
@@ -0,0 +1,44 @@
+<?php 
+
+include '../../config/conn.php';
+include '../../config/functions.php';
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $certificado = test_data($_POST['certificado']);
+    $nombre = test_data($_POST['nombre']);
+    $correo = test_data($_POST['email']);
+    $rfc = test_data($_POST['rfc']);
+    $puesto = test_data($_POST['puesto']);
+    $telefono = test_data($_POST['numero-telefonico']);
+    $direccion_fiscal = test_data($_POST['direccion-fiscal']);
+    $parametros = test_data($_POST['parametros']);
+    $estado = test_data($_POST['categoria']);
+
+    $sql = "INSERT INTO Clientes (req_certificado, nombre, rfc, nombre_contacto, puesto_contacto, correo_contacto, telefono_contacto,
+    direccion_fiscal, estado, parametros) VALUES ($certificado, '$nombre', '$rfc', '$nombre', '$puesto', 
+    '$correo', '$telefono', '$direccion_fiscal', '$estado', '$parametros')";
+    
+    $stmt = $pdo -> prepare($sql);
+    // Ejecutar la consulta
+    $stmt -> execute();
+
+    // Convertir a un arreglo asociativo
+    $res = $stmt -> fetch();
+
+    if($res){
+        header("Location: /fabrica-harinas/menu.php?error=consulta");
+        exit;
+    }else{
+        header("Location: /fabrica-harinas/menu.php?success"); 
+    }
+
+}else{
+
+    header("Location: /fabrica-harinas/menu.php");
+    exit;
+}
+
+
+?>
