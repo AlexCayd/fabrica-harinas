b41d85ebee7d157d5838d930737c9d1c31d2fdbe
emiliooarrig <earriaga@anahuac.mx>
2025-05-06 13:42:40 -0600

    Boton para mandar mail

--- modulos/historico.php
+++ modulos/historico.php
@@ -5,12 +5,15 @@ include '../config/conn.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+$estado = $_GET['estado'] ?? 1;
+
 
 // Recuperamos los datos de los certificados y los resultados de la inspección
 $sql = "SELECT 
     i.lote, 
     i.id_inspeccion, 
     c.nombre, 
+    c.correo_contacto,
     ce.cantidad_solicitada, 
     ce.cantidad_recibida,
     MIN(ri.aprobado) AS aprobado
@@ -18,6 +21,7 @@ FROM Inspeccion i
 INNER JOIN Clientes c ON i.id_cliente = c.id_cliente
 INNER JOIN Certificados ce ON ce.id_inspeccion = i.id_inspeccion
 LEFT JOIN Resultado_Inspeccion ri ON ri.id_inspeccion = i.id_inspeccion
+WHERE ri.aprobado = $estado
 GROUP BY i.id_inspeccion, i.lote, c.nombre, ce.cantidad_solicitada, ce.cantidad_recibida
 ORDER BY i.id_inspeccion DESC";
 
@@ -54,9 +58,9 @@ $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                 <div class="ordenar">
                     <h4 class="ordenar__label">Resultados</h4>
-                    <select name="categoria" id="categoria" class="ordenar__select">
-                        <option value="aprobado">Aprobado</option>
-                        <option value="desaprobado">Desaprobado</option>
+                    <select name="estado" id="ordenarPor" class="ordenar__select">
+                        <option value="1" <?php echo $estado == 1 ? 'selected' : ''; ?>>Aprobado</option>
+                        <option value="0" <?php echo $estado == 0 ? 'selected' : ''; ?>>Desaprobado</option>
                     </select>
                 </div>
 
@@ -75,6 +79,7 @@ $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         <th>Cantidad recibida (kg)</th>
                         <th>Resultados del análisis</th>
                         <th>Certificado</th>
+                        <th> Correo </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -89,11 +94,15 @@ $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             <td><?php echo $row['cantidad_recibida'] ?></td>
                             <td>
                                 <?php
-                                    echo $row['aprobado'] ? 'Aprobado' : 'Desaprobado';
+                                echo $row['aprobado'] ? 'Aprobado' : 'Desaprobado';
                                 ?>
-                                </td>
+                            </td>
                             <td>
                                 <a href="generar_pdf.php?id=<?php echo $row['id_inspeccion'] ?>" class="tabla__descargar" download>Descargar PDF</a>
+                            </td>
+
+                            <td>
+                                <a href="#" onclick="confirmarEnvio(<?php echo $row['id_inspeccion'] ?>, '<?php echo $row['correo_contacto'] ?>');" class="tabla__descargar">Enviar correo</a>
                             </td>
                         </tr>
                     <?php } ?>
@@ -104,8 +113,8 @@ $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </main>
 
     <script>
-           // Buscar por nombre
-           const buscador = document.getElementById('searchBar');
+        // Buscar por nombre
+        const buscador = document.getElementById('searchBar');
         const filasUsuarios = document.querySelectorAll('.tabla__fila');
 
         buscador.addEventListener('input', () => {
@@ -121,6 +130,41 @@ $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 }
             });
         });
+
+        // Filtros por aprobado
+        // // Script para seleccionar un estado
+        document.addEventListener("DOMContentLoaded", function() {
+            const selectEstado = document.getElementById("ordenarPor");
+
+            selectEstado.addEventListener("change", function() {
+                // alert("Opción seleccionada: " + this.value);
+                const estado = this.value;
+                // Cambiar la URL 
+                const nuevaUrl = new URL(window.location);
+                nuevaUrl.searchParams.set("estado", estado);
+                history.pushState({}, "", nuevaUrl);
+
+                location.reload();
+            });
+        });
+
+
+        function confirmarEnvio(id, correo) {
+            Swal.fire({
+                title: '¿Enviar correo?',
+                text: "¿Desea enviar el certificado por correo electrónico a: " + correo + "?",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, enviar',
+                cancelButtonText: 'Cancelar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = '../config/mandar_mail.php?id=' + id;
+                }
+            })
+        }
     </script>
 </body>
 
